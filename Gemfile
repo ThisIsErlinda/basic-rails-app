5761be05b9afcb34af60dc46d8cc2962a83ddeae
Erlinda <thisiserlinda@gmail.com>
2015-06-29 18:53:23 -0500

    seed data unique post

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
+
+gem 'faker'
 gem 'pry-rails'
 
 gem 'bootstrap-sass'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+ 
+# Create Posts
+50.times do
+ Post.create!(
+   title:  Faker::Lorem.sentence,
+   body:   Faker::Lorem.paragraph
+ )
+end
+posts = Post.all
+
+
+# Unique Post
+post = Post.where(title: "Erlinda's unique post").first 
+unless post 
+Post.create!(
+     title: "Erlinda's unique post",
+     body: "Will this work? Let's hope so."
+   )
+end
+
+
+# Create Comments
+100.times do
+ Comment.create!(
+   post: posts.sample,
+   body: Faker::Lorem.paragraph
+ )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
