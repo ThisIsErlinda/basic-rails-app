5b61aa8de9fe660b32ded2447e59072b9f49395e
Erlinda <thisiserlinda@gmail.com>
2015-09-07 12:21:42 -0500

    validating topics

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
-  resources :postsresources :topics
+  # resources :postsresources :topics
 
 
   get 'about' => 'welcome#about'
